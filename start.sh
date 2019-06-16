37dd1f9e64d7c58a82109c5f65a4e329c93500ac
kirill <semenovkirill.spb@gmail.com>
2019-06-16 14:11:33 +0200

    semi-automated flow

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@
 source credentials
 
 yandex-disk token --password=$PASSWORD $USER
-yandex-disk start --dir=/app/src/demo/data
+yandex-disk start --dir=/app/demo/data
 
 echo "Greetings! Starting your CRDT Editor"
 
-cd /app/src/ && stack exec demo
+cd /app/ && ./editor
+#/bin/bash
